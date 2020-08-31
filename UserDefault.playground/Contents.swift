7d48c38a37d413c8621e24649b916ccf8e5e7c44
MO <mo@MOs-iMac.home>
2020-08-30 23:11:51 -0400

    struct with wrapper

--- UserDefault.playground/Contents.swift
+++ UserDefault.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var str = "Hello, playground"
+
+@propertyWrapper
+struct UserDefaultsWrapper<Token> {
+    let tokenKey: String
+    let defaultValue: Token
+    let userDefaults: UserDefaults = .standard
+    
+    var wrappedValue: Token{
+        get {
+            return userDefaults.object(forKey: tokenKey) as? Token ?? defaultValue
+        }
+        set {
+            userDefaults.set(newValue, forKey: tokenKey)
+        }
+    }
+}
+
+
